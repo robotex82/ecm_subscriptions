1f9802fc8536aa3906441a4eef44983eeace5ab9
Roberto Vasquez Angel <robotex@robotex.de>
2011-07-26 20:00:36 +0200

    Added locales generator

---
+++ lib/generators/ecm/subscription/locales/locales_generator.rb
@@ -0,0 +1,18 @@
+require 'rails/generators/migration'
+
+module Ecm
+  module Subscription
+    module Generators
+      class LocalesGenerator < Rails::Generators::Base
+        desc "Generates the locale files"
+             
+        source_root File.expand_path('../../../../../../config/locales', __FILE__)
+        
+        def generate_locales
+          copy_file "ecm_subscriptions_en.yml", "config/locales/ecm_subscriptions_en.yml"
+          copy_file "ecm_subscriptions_de.yml", "config/locales/ecm_subscriptions_de.yml"
+        end      
+      end
+    end
+  end
+end        
