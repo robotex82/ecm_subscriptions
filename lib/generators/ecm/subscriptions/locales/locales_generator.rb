bad39f57f6c2370582010b5e760a59ba5a156344
Roberto Vasquez Angel <robotex@robotex.de>
2011-07-26 20:30:46 +0200

    Fixed locales

--- lib/generators/ecm/subscriptions/locales/locales_generator.rb
+++ lib/generators/ecm/subscriptions/locales/locales_generator.rb
@@ -9,8 +9,8 @@ module Ecm
         source_root File.expand_path('../../../../../../config/locales', __FILE__)
         
         def generate_locales
-          copy_file "ecm_subscriptions_en.yml", "config/locales/ecm_subscriptions_en.yml"
-          copy_file "ecm_subscriptions_de.yml", "config/locales/ecm_subscriptions_de.yml"
+          copy_file "ecm.subscriptions.en.yml", "config/locales/ecm.subscriptions.en.yml"
+          copy_file "ecm.subscriptions.de.yml", "config/locales/ecm.subscriptions.de.yml"
         end      
       end
     end
